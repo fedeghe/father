7c5d222da15703589aa52a2cf8b7ebb3c42dd005
fedeghe <fedeghe@gmail.com>
2018-11-25 23:30:11 +0100

    ingore umd coverage to get back to 100%

--- source/index.js
+++ source/index.js
@@ -1,10 +1,11 @@
 (function (root, factory) {
+    /* istanbul ignore next */
     if (typeof exports === 'object') {
         module.exports = factory();
-    // } else if (typeof define === 'function' && define.amd) {
-    //     // AMD
-    //     define([], factory);
+    } else if (typeof define === 'function' && define.amd) {
+        define([], factory);
     } else {
+        
         root.Balle = factory();
     }
 }(this, function () {
